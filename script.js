c17651f1193361e46709e10fd9c48100151e3ccc
MeliEiros <meelieiros@gmail.com>
2023-07-18 16:29:06 -0300

    pequeños cambios

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-//const aside = document.getElementById ('panel');//
-//const textAside = document.getElementById ('panel'); 
-//const hideText = () => {
-//hideText.classlist.add("hidden");
-}//
+alert('hola')
+
+//const aside = document.getElementById ('panel');
+//const textAside = document.getElementById ('panel'); //
+//const hideText = () => {//
+//hideText.classlist.add("hidden");//
+
+//const textAside = document.getElementById("panel-hidden");
+
+
 const urlImg = document.getElementById('url-img-input')
 const memeImg = document.getElementById('container-meme')
 UrlImgInput.addEventListener('input', (e)=> changeBackground(e))
 
 const changeBackground =(e) => { 
     console.log(e.target.value)
-    memeImg.style.backgroundImage = 'url(${e.target.value})' 
-}
+    memeImg.style.backgroundImage = `url("${UrlImgInput.value}"`;     //'url(' ${e.target.value}')'// 
+}) 
