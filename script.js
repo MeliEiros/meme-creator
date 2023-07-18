8cd56ebfb88366c16f17c41d75fe514409b1214f
MeliEiros <meelieiros@gmail.com>
2023-07-18 00:26:13 -0300

    js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //const aside = document.getElementById ('panel');//
-const textAside = document.getElementById ('panel'); 
-const hideText = () => {
+//const textAside = document.getElementById ('panel'); 
+//const hideText = () => {
+//hideText.classlist.add("hidden");
+}//
+const urlImg = document.getElementById('url-img-input')
+const memeImg = document.getElementById('container-meme')
+UrlImgInput.addEventListener('input', (e)=> changeBackground(e))
 
+const changeBackground =(e) => { 
+    console.log(e.target.value)
+    memeImg.style.backgroundImage = 'url(${e.target.value})' 
 }
