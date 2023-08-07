a97e325b9382b1e8bfa2fc8fa6438cff902d4090
MeliEiros <meelieiros@gmail.com>
2023-08-07 20:56:56 -0300

    imagen y asides

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-alert('hola')
-
- const buttonText = document.getElementById("img-button");
+const buttonText = document.getElementById("img-button");
  const buttonImg = document.getElementById("text-button");
 
  const aside = document.getElementById("aside-img");
  const asideText =document.getElementById("aside-text");
 
  const textHide = () => {
-    aside.classList.add("hidden");
-    asideText.classList.remove("hidden");
+    aside.classList.add("panel-hidden");
+    asideText.classList.remove("panel-hidden2");
  }
  const imgHide = () => { 
-    asideText.classList.add("hidden");
-    aside.classList.remove("hidden");
+    asideText.classList.add("panel-hidden2");
+    aside.classList.remove("panel-hidden");
+    
  }
-buttonText.addEventListener("click" , () => imgHide());
-buttonImg.addEventListener("click" , () => textHide());
+buttonText.addEventListener("click" , () => textHide());
+buttonImg.addEventListener("click" , () => imgHide());
 
 const buttonMood = document.getElementById("mood-button");
 const main = document.getElementById("main-container");
@@ -36,20 +35,15 @@ const tittleColor = document.getElementById("tittle-color");
 const colorBack = document.getElementById("color-background");
 
 const cambiarModo = () => {
-   aside.classList.toggle("light-aside")
+   aside.classList.toggle("light-aside");
+   asideText.classList.toggle("light-aside");
 }
 
-//const aside = document.getElementById ('panel');
-//const textAside = document.getElementById ('panel'); //
-//const hideText = () => {//
-//hideText.classlist.add("hidden");//
-
-//const textAside = document.getElementById("panel-hidden");
 
 
 const urlImg = document.getElementById ("url-img-input")
 const memeImg = document.getElementById("container-meme")
-UrlImgInput.addEventListener('input', (e)=> changeBackground(e))
+urlImg.addEventListener('input', (e)=> changeBackground(e))
 
 const changeBackground = (e) => { 
     console.log(e.target.value)
